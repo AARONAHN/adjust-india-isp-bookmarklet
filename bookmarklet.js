0a6a6b5600492af3fa9599f2daf7721468d5db6b
Aaron <aaron@noraa.me>
2021-09-29 14:15:07 +0900

    update selector

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,10 @@
 javascript: (function () {
   function urlChange() {
     const clickUrl = document.querySelector(
-      "#sidebar-view > div:nth-child(2) > div > div > div > div.content-inner.flex-one.flex-box-column > div > div.tracker-edit-options > div:nth-child(1) > div.url"
+      "#sidebar-view > div:nth-child(2) > div > div > div > div.content-inner.flex-one.flex-box-column > div > div.scrollable.flex-one.flex-box-column > div.form-row.tracker-edit-options > div:nth-child(1) > div > div.url"
     );
     const impUrl = document.querySelector(
-      "#sidebar-view > div:nth-child(2) > div > div > div > div.content-inner.flex-one.flex-box-column > div > div.tracker-edit-options > div:nth-child(2) > div.url"
+      "#sidebar-view > div:nth-child(2) > div > div > div > div.content-inner.flex-one.flex-box-column > div > div.scrollable.flex-one.flex-box-column > div.form-row.tracker-edit-options > div.form-row.form-row-tight.form-row-url-display > div.url"
     );
     clickUrl.innerText = clickUrl.innerText.replace(
       "app.adjust.com",
